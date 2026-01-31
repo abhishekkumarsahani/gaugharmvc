17e29a064f09af0b334907527461189f055e9b83
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2026-01-31 22:08:28 +0545

    Create Features Added

---
+++ Views/Cow/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GauGhar.Views.Cow
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
